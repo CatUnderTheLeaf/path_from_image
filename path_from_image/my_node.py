052a87f26d4f6ee246486eff8d7f23b9f43f9d48
CatUnderTheLeaf <stacy.chaikovska@gmail.com>
2022-05-31 15:35:29 +0200

    init ros package

---
+++ path_from_image/my_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from path_from_image.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+package_name = 'path_from_image'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=[
+        'setuptools',
+        'numpy',
+        'sympy'
+    ],
+    zip_safe=True,
+    maintainer='Stacy',
+    maintainer_email='catundertheleaf@gmail.com',
+    description='A ROS package, which gets real world lane coordinates with only image, camera calibration and position info.',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'my_node = path_from_image.my_node:main'
+        ],
+    },
+)
