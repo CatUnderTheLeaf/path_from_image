569b7c5540d302aecab1c4a4fb0b4b64bef0ecd2
CatUnderTheLeaf <stacy.chaikovska@gmail.com>
2022-07-20 13:20:34 +0200

    publish correct top-view perspective of an image

--- path_from_image/image_warper.py
+++ path_from_image/image_warper.py
@@ -1,22 +1,21 @@
 import numpy as np
 import cv2
+from sympy import Point3D, Line3D, Plane
+
 import rclpy
-import threading
 from rclpy.node import Node
 from sensor_msgs.msg import Image, CameraInfo
 from geometry_msgs.msg import PoseStamped
 # !!! very important import !!!
-# without it Buffer.transform doesn't work
+# without it Buffer.transform() doesn't work
 import tf2_geometry_msgs
-from cv_bridge import CvBridge, CvBridgeError
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
-from tf2_ros import TransformException
 from tf2_ros import LookupException
 import tf_transformations
-from sympy import Point3D, Line3D, Plane
 
 from image_geometry import PinholeCameraModel
+from cv_bridge import CvBridge, CvBridgeError
 
 class ImageWarper(Node):
 
@@ -28,7 +27,6 @@ class ImageWarper(Node):
         self.tf_listener = TransformListener(self._tf_buffer, self)
         # Call on_timer function every second
         self._output_timer = self.create_timer(1.0, self.on_timer)
-        self._lock = threading.Lock()
         self._tf_future = None
         # TODO get from params
         self._camera_frame = 'camera_link_optical'
@@ -43,56 +41,40 @@ class ImageWarper(Node):
         self.transformMatrix = None
         # scale factors, meters per image height/width
         self.x_scale = None
+        self.y_scale = None
         # TODO 
-        # get scale parameter from params server
-        # y_scale = rospy.get_param('~y_scale')
-        self.y_scale = 10.0
-
-
-        # mock of CameraInfo
-        info = CameraInfo()
-        info.header.frame_id = 'front_camera_sensor'
-        info.height = 800
-        info.width = 800
-        info.distortion_model = 'plumb_bob'
-        info.d = [0.0, 0.0, 0.0, 0.0, 0.0]
-        info.k = [476.7030836014194, 0.0, 400.5, 0.0, 476.7030836014194, 400.5, 0.0, 0.0, 1.0]
-        info.p = [476.7030836014194, 0.0, 400.5, -33.36921585209936, 0.0, 476.7030836014194, 400.5, 0.0, 0.0, 0.0, 1.0, 0.0]
-        info.r = [1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0]
-        info.binning_x = 0
-        info.binning_y = 0
-        info.roi.x_offset = 0
-        info.roi.y_offset = 0
-        info.roi.height = 0
-        info.roi.width = 0
-        info.roi.do_rectify = False
-        self.camera_model.fromCameraInfo(info)
-        self.cameraInfoSet = True
-
-
-
+        # get these 2 parameters from params server
+        # eg. distance_ahead = rospy.get_param('~distance_ahead')
+        self.distance_ahead = 10.0
+        self.lane_width = 10.0
+        
         self.bridge = CvBridge()
 
         # Publishers and subscribers
-        # self.camera_sub = self.create_subscription(
-        #     Image,
-        #     '/vehicle/front_camera/image_raw',
-        #     self.camera_callback,
-        #     10)
-        # self.camera_sub
-        # self.img_pub = self.create_publisher(
-        #     Image,
-        #     '/wrap_img',
-        #     10)
-        # self.img_pub
-        # self.camera_info_sub = self.create_subscription(
-        #     CameraInfo,
-        #     '/vehicle/front_camera/camera_info',
-        #     self.info_callback,
-        #     1)
-        # self.camera_info_sub
+        self.camera_sub = self.create_subscription(
+            Image,
+            '/vehicle/front_camera/image_raw',
+            self.camera_callback,
+            10)
+        self.camera_sub
+        self.img_pub = self.create_publisher(
+            Image,
+            '/wrap_img',
+            10)
+        self.img_pub
+        self.camera_info_sub = self.create_subscription(
+            CameraInfo,
+            '/vehicle/front_camera/camera_info',
+            self.info_callback,
+            1)
+        self.camera_info_sub
         
     def on_tf_ready(self, future):
+        """ set transformation matrix when tf-transform is ready
+
+        Args:
+            future (Future): the outcome of a transform-task in the future
+        """        
         self._tf_future = None
         if (future.result() and self.cameraInfoSet):
             try:
@@ -100,10 +82,11 @@ class ImageWarper(Node):
             except LookupException:
                 self.get_logger().info('transform no longer available')
             else:
-                # b = trans.transform.translation
                 self.get_logger().info('Got transform')
 
     def on_timer(self):
+        """ call and wait for tf-transformation to be ready
+        """        
         self.get_logger().info('on_timer function called')
         if self._tf_future:
             self.get_logger().info('Still waiting for transform')
@@ -119,15 +102,15 @@ class ImageWarper(Node):
                 self._base_frame, self._camera_frame))
    
     def camera_callback(self, msg):
+        """ get picture and publish its top-view perspective
+
+        Args:
+            msg (Image): ros image message
+        """        
         if self.transformMatrix is not None:
             self.get_logger().info('--------------I have already transform matrix and can transform image:')
             try:
                 cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-                # import os
-                # import cv2
-                # test_dir_path = '/home/catundertheleaf/robocar/robocar_ws/src/path_from_image/test'
-                # self.get_logger().info(test_dir_path)
-                # cv2.imwrite(os.path.join(test_dir_path, 'cv_image.jpg'), cv_image)
                 warp_img = self.warp(cv_image)
                 # img type is 8UC4 not compatible with bgr8
                 top_view_msg = self.bridge.cv2_to_imgmsg(warp_img, "bgr8")
@@ -138,7 +121,12 @@ class ImageWarper(Node):
         else:
             self.get_logger().info('--------------There is no transform matrix:')
 
-    def info_callback(self, msg):        
+    def info_callback(self, msg):   
+        """ get camera info and load it to the camera model
+
+        Args:
+            msg (CameraInfo): ros camera info message
+        """             
         if not self.cameraInfoSet:
             self.get_logger().info('I heard first cameraInfo------------------')
             self.camera_model.fromCameraInfo(msg)
@@ -216,7 +204,10 @@ class ImageWarper(Node):
             zero = self.transformPoint((0.,0.,0.), fromCamera=True)
             lbc_point = self.transformPoint(lbc_ray, fromCamera=True)
             rbc_point = self.transformPoint(rbc_ray, fromCamera=True)            
-            point3, point4, x_scale = self.getUpperPoints(zero, lbc_point, rbc_point)
+            point3, point4, x_scale = self.getUpperPoints(zero, lbc_point, rbc_point)   
+            # set scale factors in meters/pixel
+            self.x_scale = x_scale/w
+            self.y_scale = self.distance_ahead/h
             # transform points 3 and 4 to camera_optical_link frame
             luc_point = self.transformPoint(point3, fromCamera=False)
             ruc_point = self.transformPoint(point4, fromCamera=False)
@@ -226,32 +217,14 @@ class ImageWarper(Node):
             # right upper corner        
             ruc = self.camera_model.project3dToPixel(ruc_point)
             # form src and dst points
-            # TODO make rotation to the pose, because x axes are not in the same directions
-            self.get_logger().info("zero point in base frame")
-            self.get_logger().info(f'zero {float(zero[0])}, {float(zero[1])}, {float(zero[2])}')
-            self.get_logger().info("lbc and rbc in base frame")
-            self.get_logger().info(f'lbc {float(lbc_point[0])}, {float(lbc_point[1])}, {float(lbc_point[2])}')
-            self.get_logger().info(f'rbc {float(rbc_point[0])}, {float(rbc_point[1])}, {float(rbc_point[2])}')
-            self.get_logger().info("luc and ruc in base frame")
-            self.get_logger().info(f'luc {float(point3[0])}, {float(point3[1])}, {float(point3[2])}')
-            self.get_logger().info(f'ruc {float(point4[0])}, {float(point4[1])}, {float(point4[2])}')
-            # self.get_logger().info(f'approx luc {w/3}, {h/2}')
-            # self.get_logger().info(f'approx ruc {2*w/3}, {h/2}')
+            # left upper corner, right upper corner, right bottom corner, left bottom corner
             src = [[luc[0], luc[1]],[ruc[0], ruc[1]],[w, h],[0, h]]
-            # src = [[w/3, h/2],[2*w/3, h/2],[w, h],[0, h]]
-            dst = [[0, 0],[w, 0],[w, h],[0, h]]
-            # dst = [[0, 0],[w, 0],[2*w/3, h],[w/3, h]]
+            # new x coordinates are scaled with lane_width
+            l_w = w/2*(1-x_scale/self.lane_width)
+            r_w = w/2*(1+x_scale/self.lane_width)
+            dst = [[l_w, 0],[r_w, 0],[r_w, h],[l_w, h]]
             if (src and dst):
                 self.transformMatrix = self.get_transform_matrix(src, dst)
-                # self.x_scale = x_scale/w
-                # self.y_scale = y_scale/h
-                # TODO
-                # mock transform image
-                test_path_in = '/home/catundertheleaf/robocar/robocar_ws/src/path_from_image/path_from_image/cv_image.jpg'
-                img = cv2.imread(test_path_in)
-                out_img = self.warp(img)
-                test_path_out = '/home/catundertheleaf/robocar/robocar_ws/src/path_from_image/path_from_image/wraped_image.jpg'
-                cv2.imwrite(test_path_out, out_img)
             return    
    
     def get_transform_matrix(self, src, dst):
@@ -292,21 +265,23 @@ class ImageWarper(Node):
         point1 = xy_plane.intersection(lbc_line)[0]
         point2 = xy_plane.intersection(rbc_line)[0]
 
-        # translate factor in meters
-        # depends on h*w of the image
-        # and scale factor
+        # distance in meters between bottom points
+        # after their projection onto the ground plane
         x_scale = float(point1.distance(point2))
 
         # get upper points in the robot/world frame
         # sometimes in car models front camera 
         # looks in the negative y-axe direction
         # so our y-translation should has right sign
+        # TODO maybe has to be some way to determine 
+        # which axis goes along the car model and
+        # if it has the same direction as z-axis of the cv2-image
         if lbc[1] > 0:
             sign = 1
         else:
             sign = -1
-        point3 = point1.translate(0, sign*self.y_scale)
-        point4 = point2.translate(0, sign*self.y_scale)
+        point3 = point1.translate(0, sign*self.distance_ahead)
+        point4 = point2.translate(0, sign*self.distance_ahead)
         
         return point3, point4, x_scale
 
